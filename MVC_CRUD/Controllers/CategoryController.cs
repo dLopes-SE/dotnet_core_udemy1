6e038190c0e476e2751b3099b07a07722435365d
dlopes <dylananlopes@gmail.com>
2022-03-26 17:51:51 +0000

    MVC_CRUD updates 0

---
+++ MVC_CRUD/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD.Data;
+using MVC_CRUD.Models;
+
+namespace MVC_CRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
+        }
+    }
+}
