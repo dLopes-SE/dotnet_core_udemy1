133ad1bdd82d409bba2df9c0000362da15e75c8b
dlopes <dylananlopes@gmail.com>
2022-04-03 15:41:44 +0100

    Implemented N-tier architecture

---
+++ MVC_CRUD/MVC_CRUD.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_CRUD.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = String.Empty;
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ MVC_CRUD/MVC_CRUD/Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_CRUD.Data;
+using MVC_CRUD.Models;
+
+namespace MVC_CRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories
+                                        .ToList()
+                                        .OrderBy(c => c.DisplayOrder)
+                                        .ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (_db.Categories.ToList().
+                Select(c => c.DisplayOrder).
+                Contains(obj.DisplayOrder))
+            {
+                string error = "Cannot have same display order";
+                ModelState.AddModelError("CustomError", error);
+                TempData["error"] = error;
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Created object with success!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return NotFound();  
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            var category = _db.Categories.ToList();
+            if (ModelState.IsValid)
+            {
+                if (!category.Select(c => c.DisplayOrder).Contains(obj.DisplayOrder))
+                {
+                    _db.Categories.Update(obj);
+                    _db.SaveChanges();
+                    TempData["success"] = "Object updated with success!";
+                    return RedirectToAction("Index");
+                }
+                TempData["error"] = "Cannot have same display order";
+                return View(obj);
+            }
+            TempData["error"] = "Model isn't valid";
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (obj == null)
+                return NotFound();
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Deleted object with success!";
+            return RedirectToAction("Index");
+        }
+    }
+}
